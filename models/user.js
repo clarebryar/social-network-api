958b9d53f524150ebccfa95f010b6d7f9057447f
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-08-07 12:47:10 -0500

    user schema

--- models/user.js
+++ models/user.js
@@ -1,12 +1,45 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, Types } = require("mongoose");
 
-const userSchema = new Schema (
-    {
-        userName: {
-            type: String, 
-            unique: true, 
-            required: true,
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      // mongoose validation?
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
 
-        }
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
-})
+postSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+
+const User = model("user", userSchema);
+
+module.exports = User;
